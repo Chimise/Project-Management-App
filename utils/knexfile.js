2f653af7d3c5d7aafaf77cf458a3b29a568f21db
chisom promise <chimisepro@gmail.com>
2022-12-01 11:19:10 +0100

    Preparing for production

--- utils/knexfile.js
+++ utils/knexfile.js
@@ -1,23 +1,26 @@
-const { loadEnvConfig } = require('@next/env')
-const path = require('path');
+const { loadEnvConfig } = require("@next/env");
+const path = require("path");
 
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== "production";
 
-const { DB_USER, DB_PASSWORD, DB_NAME } = loadEnvConfig(path.resolve('../'), dev).combinedEnv;
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_URL } = loadEnvConfig(
+  path.resolve("../"),
+  dev
+).combinedEnv;
 
 module.exports = {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-        port: 5432,
-        user: DB_USER,
-        password: DB_PASSWORD,
-        database: DB_NAME
-  },
+  client: "pg",
+  connection: process.env.NODE_ENV === 'production' ? DB_URL : ({
+    host: "127.0.0.1",
+    port: 5432,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+  }),
   migrations: {
-    directory: '../knex/migrations',
+    directory: "../knex/migrations",
   },
   seeds: {
-    directory: '../knex/seeds',
+    directory: "../knex/seeds",
   },
-}
+};
