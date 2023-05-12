c49c7e58840038cd9ad238a5333b2b70259060fc
chisom promise <chimisepro@gmail.com>
2023-05-12 01:30:58 +0100

    feat: Add more stats to dashboard and update docs

--- README.md
+++ README.md
@@ -1,9 +1,40 @@
 # Project Management App
 
-A fullstack application for creating and tracking the state of your tasks for your new projects with support for user authentication as well as adding comments to task, viewing reports and messages.
+A fullstack application for creating and managing the lifecycle of your tasks for your projects from the creation to the completion stage.
 
-## Demo
-Here is a working live demo: [https://taskr-beryl.vercel.app/](https://taskr-beryl.vercel.app/)
+**Live Demo**: [https://taskr-beryl.vercel.app/](https://taskr-beryl.vercel.app/)
+
+**Screenshot - Dashboard**
+![Dashboard](./docs/dashboard.png)
+
+**Screenshot - Projects**
+![Projects](./docs/projects.png)
+
+**Screenshot - Project-Details**
+![Create Tasks](./docs/add-task.png)
+
+**Screenshots - Messages**
+![Messages](./docs/messages.png)
+
+## Features
+- View statistics about created projects and state of created tasks.
+- Create new project and tasks, update or delete them and track the lifecycle of tasks for a project.
+- Add comments to created tasks, mark them as favourites or like them.
+- Show new reports or messages and view them only once.
+- Caching and deduping of API request to the backend resulting in a better performance
+- Optimistic UI updates leading to a better user experience.
+- Prevent loss of data when creating or updating task which can occur by reloading the page or going to new pages by showing confirmation popups.
+- Secure authentication with JWT Token and authorization.
+- Update user information like name and password.
+
+## Technologies Used
+This Project is built with:
+* [Next.js](https://nextjs.org/)
+* [Postgres](https://www.postgresql.org/)
+* [Tailwind CSS](https://tailwindcss.com/)
+* [Framer Motion](https://www.framer.com/)
+* [Knex](https://knexjs.org/)
+* [TypeScript](https://www.typescriptlang.org/)
 
 ## Usage
 To run the project locally, follow these steps below:
@@ -15,11 +46,3 @@ To run the project locally, follow these steps below:
 * Visit [http://localhost:3000](http://localhost:3000) to view the homepage of the web application.
 * You can also run all the test files by running ```npm test```.
 
-## Technologies
-Project was created with:
-* [Next.js](https://nextjs.org/)
-* [Postgres](https://www.postgresql.org/)
-* [Tailwind CSS](https://tailwindcss.com/)
-* [Framer Motion](https://www.framer.com/)
-* [Knex](https://knexjs.org/)
-* [TypeScript](https://www.typescriptlang.org/)
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Container from "../../components/ui/Container";
@@ -9,6 +10,15 @@ import useUser from "../../hooks/useUser";
 import useProjects from "../../hooks/useProjects";
 import useReports from "../../hooks/useReports";
 import useMessages from "../../hooks/useMessages";
+import { Task } from "../../hooks/useAddTask";
+
+const displayTasks = (tasks: Task[] | undefined, status: string) => {
+  if(!tasks) {
+    return null;
+  }
+  const isMultiple = tasks.length > 1;
+  return `${tasks.length} ${status} ${isMultiple ? 'tasks' : 'task'}`
+}
 
 const DashboardPage = () => {
   const { firstName } = useUser();
@@ -16,6 +26,26 @@ const DashboardPage = () => {
   const { projects } = useProjects();
   const { unRead: unReadMessages } = useReports();
   const { unRead: unReadReports } = useMessages();
+
+  const allTasks = useMemo(() => {
+    if(!projects) {
+      return;
+    }
+    return projects.reduce<Array<Task>>((tasks, project) => tasks.concat(project.tasks), []);
+
+  }, [projects]);
+
+  const createdTasks = useMemo(() => {
+    return allTasks?.filter(task => task.status === 0);
+  }, [allTasks])
+
+  const progressTasks = useMemo(() => {
+    return allTasks?.filter(task => task.status === 1);
+  }, [allTasks])
+
+  const completedTasks = useMemo(() => {
+    return allTasks?.filter(task => task.status === 2);
+  }, [allTasks])
 
   return (
     <Container>
@@ -45,9 +75,10 @@ const DashboardPage = () => {
           className="h-60 md:h-auto md:row-span-2"
         >
           <Card img="project" className="w-full h-full">
+            <div className="space-y-2 lg:space-y-3">
             <IconList icon="dashboard">
               <span className="text-gray-500 group-hover:text-gray-700 tracking-wide text-base font-light">
-                You{`'`}ve got{" "}
+                You{`'`}ve {" "}
                 <span
                   id="dashboard"
                   className="text-gray-700 font-medium group-hover:text-black"
@@ -61,6 +92,40 @@ const DashboardPage = () => {
                 </span>
               </span>
             </IconList>
+            <IconList icon="created">
+              <span className="text-gray-500 group-hover:text-gray-700 tracking-wide text-base font-light">
+                You{`'`}ve {" "}
+                <span
+                  id="dashboard"
+                  className="text-gray-700 font-medium group-hover:text-black"
+                >
+                  {displayTasks(createdTasks, 'created')}
+                </span>
+              </span>
+            </IconList>
+            <IconList icon="progress">
+              <span className="text-gray-500 group-hover:text-gray-700 tracking-wide text-base font-light">
+                You{`'`}ve {" "}
+                <span
+                  id="dashboard"
+                  className="text-gray-700 font-medium group-hover:text-black"
+                >
+                  {displayTasks(progressTasks, 'ongoing')}
+                </span>
+              </span>
+            </IconList>
+            <IconList icon="completed">
+              <span className="text-gray-500 group-hover:text-gray-700 tracking-wide text-base font-light">
+                You{`'`}ve {" "}
+                <span
+                  id="dashboard"
+                  className="text-gray-700 font-medium group-hover:text-black"
+                >
+                  {displayTasks(completedTasks, 'completed')}
+                </span>
+              </span>
+            </IconList>
+            </div>
           </Card>
         </div>
         <div
@@ -72,7 +137,7 @@ const DashboardPage = () => {
           <Card img="report" className="w-full h-full">
             <IconList icon="report">
               <span className="text-gray-500 group-hover:text-gray-700 tracking-wide text-base font-light">
-                You{`'`}ve got{" "}
+                You{`'`}ve {" "}
                 <span
                   className="text-gray-700 font-medium group-hover:text-black"
                   id="report"
@@ -93,7 +158,7 @@ const DashboardPage = () => {
           <Card img="message" className="w-full h-full">
             <IconList icon="message">
               <span className="text-gray-500 group-hover:text-gray-700 tracking-wide text-base font-light">
-                You{`'`}ve got{" "}
+                You{`'`}ve {" "}
                 <span
                   className="text-gray-700 font-medium group-hover:text-black"
                   id="message"
